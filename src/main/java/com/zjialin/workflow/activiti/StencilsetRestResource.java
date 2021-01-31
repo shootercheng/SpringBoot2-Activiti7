3fe3efefe65f14b5443aa5ab49e4e5abc875cae0
chengdu <3281328128@qq.com>
2021-01-31 11:52:56 +0800

    update resource address

--- src/main/java/com/zjialin/workflow/activiti/StencilsetRestResource.java
+++ src/main/java/com/zjialin/workflow/activiti/StencilsetRestResource.java
@@ -17,7 +17,7 @@ public class StencilsetRestResource {
     public String getStencilset() {
         log.info("StencilsetRestResource.getStencilset-----------");
         // 文件位置需要跟stencilset.json文件放置的路径匹配,否则进入到在线编辑器页面会是一片空白,没有菜单等显示信息
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("activiti/stencilset.json");
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/stencilset.json");
         try {
             return IOUtils.toString(inputStream, "utf-8");
         } catch (Exception e) {
